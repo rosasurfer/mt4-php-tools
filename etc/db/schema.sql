d90a3fdbdf8de987fede7f1a7a65fccf3827c767
Peter Walther <pewa-github@rosasurfer.com>
2010-11-24 21:21:13 +0000

    *** empty log message ***

--- etc/db/schema.sql
+++ etc/db/schema.sql
@@ -1,80 +1,66 @@
 /*
 Created     23.05.2009
-Modified    15.10.2009
-Project     FxTrader
+Modified    24.11.2010
+Project     Forex
 Model       
 Company     
 Author      Peter Walther
-Version     0.1
+Version     
 Database    MySQL 5
 */
 
 
 drop database if exists fxtrader;
-create database fxtrader;
+create database fxtrader character set latin1;
 use fxtrader;
 
 
 create table t_account (
    id int unsigned not null auto_increment,
    created datetime not null,
-   name varchar(100) not null,
-   currency char(3) not null comment 'Kontowährung',
-   commission decimal(7,6) unsigned not null default 0 comment 'Rate je Einheit der Kontowährung',
-   primary key (id)
-) engine = InnoDB;
-
-
-create table t_execution (
-   id int unsigned not null auto_increment,
-   created datetime not null,
-   strategy varchar(100) not null default '' comment 'System, das das Signal ausgelöst hat',
-   type enum('b','s') not null comment 'buy/sell',
-   size int unsigned not null,
-   symbol char(7) not null,
-   price decimal(7,4) unsigned not null,
-   commission decimal(10,2) unsigned not null default 0 comment 'denormalisiert (leitet sich aus der Menge, dem Konto und ggf. dem Instrument ab)',
-   account_id int unsigned not null,
+   company varchar(100) not null comment 'account company',
+   number int unsigned not null comment 'account number',
+   currency char(3) not null comment 'base currency',
+   lotsize int unsigned not null comment 'lotsize in base units',
+   mtiaccount varchar(50) comment 'MTI Live account id',
    primary key (id),
-   index i_account_id (account_id),
-   constraint execution_account_id foreign key (account_id) references t_account (id) on delete restrict on update cascade
+   unique key u_company_number (company,number),
+   unique key u_mtiaccount (mtiaccount)
 ) engine = InnoDB;
 
-
-create table t_trade (
+create table t_order (
    id int unsigned not null auto_increment,
    created datetime not null,
-   symbol char(7) not null,
-   type enum('l','s') not null comment 'long/short',
-   size int unsigned not null,
+   type enum('balance','credit','trade') not null comment 'balance | credit | trade',
+   ticket int unsigned not null,
+   symbol char(12),
+   direction enum('long','short') comment 'long | short',
+   lots decimal(10,2) unsigned not null,
    opened datetime not null,
-   openprice decimal(7,4) unsigned not null,
-   closed datetime,
-   closeprice decimal(7,4) unsigned,
-   commission decimal(10,2) unsigned not null default 0 comment 'denormalisiert (leitet sich aus der Menge, dem Konto und ggf. dem Instrument ab)',
+   openprice decimal(10,5) unsigned not null,
+   takeprofit decimal(10,5) unsigned,
+   stoploss decimal(10,5) unsigned,
+   closed datetime not null,
+   closeprice decimal(10,5) unsigned not null,
+   commission decimal(10,2) not null,
+   swap decimal(10,2) not null,
+   netprofit decimal(10,2) not null,
+   grossprofit decimal(10,2) not null,
+   pips decimal(5,1) not null,
+   result enum('win','loss','breakeven','n/a') not null comment 'win | loss | breakeven | n/a',
+   duration int unsigned not null comment 'trade duration in minutes',
+   magicnumber int unsigned not null,
    comment varchar(100),
    account_id int unsigned not null,
    primary key (id),
+   unique key u_account_id_ticket (account_id,ticket),
    index i_account_id (account_id),
-   constraint trade_account_id foreign key (account_id) references t_account (id) on delete restrict on update cascade
-) engine = InnoDB;
-
-
-create table t_ticks (
-   id int unsigned not null auto_increment,
-   margin decimal(10,2) unsigned not null,
-   freemargin decimal(10,2) unsigned not null,
-   datetime datetime not null,
-   ask decimal(10,5) unsigned not null,
-   bid decimal(10,5) unsigned not null,
-   symbol varchar(20) not null,
-   equity decimal(10,2) unsigned not null,
-   primary key (id)
+   constraint order_account_id foreign key (account_id) references t_account (id) on delete  restrict on update cascade
 ) engine = InnoDB;
 
 
 -- Daten einlesen
-source data.sql;
+-- source data.sql;
 
 commit;
 
