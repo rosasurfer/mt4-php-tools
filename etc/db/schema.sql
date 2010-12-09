3fa3665f8439357bdade599c3d0d61e30e47a65d
Peter Walther <pewa-github@rosasurfer.com>
2010-12-09 20:55:35 +0000

    *** empty log message ***

--- etc/db/schema.sql
+++ etc/db/schema.sql
@@ -1,6 +1,6 @@
 /*
 Created     23.05.2009
-Modified    24.11.2010
+Modified    25.11.2010
 Project     Forex
 Model       
 Company     
@@ -22,40 +22,38 @@ create table t_account (
    number int unsigned not null comment 'account number',
    currency char(3) not null comment 'base currency',
    lotsize int unsigned not null comment 'lotsize in base units',
-   mtiaccount varchar(50) comment 'MTI Live account id',
+   mtiaccount varchar(50) comment 'MTi account id',
    primary key (id),
    unique key u_company_number (company,number),
    unique key u_mtiaccount (mtiaccount)
 ) engine = InnoDB;
 
+
 create table t_order (
    id int unsigned not null auto_increment,
    created datetime not null,
-   type enum('balance','credit','trade') not null comment 'balance | credit | trade',
    ticket int unsigned not null,
-   symbol char(12),
-   direction enum('long','short') comment 'long | short',
+   type enum('buy','sell','balance','credit','vendormatching') not null comment 'buy | sell | balance | credit | vendor matching',
    lots decimal(10,2) unsigned not null,
-   opened datetime not null,
+   symbol char(12),
+   opentime datetime not null,
    openprice decimal(10,5) unsigned not null,
-   takeprofit decimal(10,5) unsigned,
-   stoploss decimal(10,5) unsigned,
-   closed datetime not null,
+   closetime datetime not null,
    closeprice decimal(10,5) unsigned not null,
    commission decimal(10,2) not null,
    swap decimal(10,2) not null,
    netprofit decimal(10,2) not null,
    grossprofit decimal(10,2) not null,
-   pips decimal(5,1) not null,
    result enum('win','loss','breakeven','n/a') not null comment 'win | loss | breakeven | n/a',
+   pips decimal(5,1) not null comment 'normalized result',
    duration int unsigned not null comment 'trade duration in minutes',
-   magicnumber int unsigned not null,
+   magicnumber int unsigned,
    comment varchar(100),
    account_id int unsigned not null,
    primary key (id),
    unique key u_account_id_ticket (account_id,ticket),
    index i_account_id (account_id),
-   constraint order_account_id foreign key (account_id) references t_account (id) on delete  restrict on update cascade
+   constraint order_account_id foreign key (account_id) references t_account (id) on delete restrict on update cascade
 ) engine = InnoDB;
 
 
