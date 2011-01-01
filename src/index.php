a94fcdfa92e0d07b3d76cbe1d52b00d2d0ed3e94
Peter Walther <pewa-github@rosasurfer.com>
2011-01-01 22:52:00 +0000

    *** empty log message ***

--- src/index.php
+++ src/index.php
@@ -4,5 +4,22 @@ include(dirName(__FILE__).'/WEB-INF/classes/classes.php');
 
 define('APPLICATION_NAME', 'fx.pewasoft');
 
+/*
+date_default_timezone_set('America/New_York');
+$date = strToTime('2010-11-02 17:00:00');
+
+date_default_timezone_set('Europe/Sofia');
+echoPre('Sofia(DATE_RFC1123) = '.date(DATE_RFC1123, $date));
+
+date_default_timezone_set('Europe/London');
+echoPre('London(DATE_RFC1123) = '.date(DATE_RFC1123, $date));
+
+date_default_timezone_set('GMT');
+echoPre('GMT(DATE_RFC1123) = '.date(DATE_RFC1123, $date));
+
+date_default_timezone_set('America/New_York');
+echoPre('New York(DATE_RFC1123) = '.date(DATE_RFC1123, $date));
+*/
+
 FrontController ::processRequest();
 ?>
