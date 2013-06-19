03ed2ab5ee182d1c5e53a026d382c0018dfbd0cb
Peter Walther <pewa-github@rosasurfer.com>
2013-06-19 09:29:22 +0000

    *** empty log message ***

--- src/WEB-INF/bin/metatrader/writeBalanceHst.php
+++ src/WEB-INF/bin/metatrader/writeBalanceHst.php
@@ -69,7 +69,7 @@ foreach ($lines as $i => &$line) {
       continue;
    }
    $values = explode("\t", $line);                                         // Spaltenanzahl prüfen
-   if (sizeOf($values) != 22)                                              exit("Invalid file format (unexpected number of columns in line ".($i+1).")\n");
+   if (sizeOf($values) != 22) exit("Invalid file format (unexpected number of columns in line ".($i+1).")\n");
 
    // MT4-Zeiten in FXT konvertieren und entsprechende GMT-Timestamps berechnen (MetaTrader versteht nur GMT)
    $date = new DateTime(gmDate('Y-m-d H:i:s', $values[11]), $serverTimezone);
@@ -82,7 +82,7 @@ foreach ($lines as $i => &$line) {
                          strToTime($date->format('Y-m-d H:i:s \G\M\T')),   // GMT-Timestamp der FXT der ServerTime; FXT = America/New_York+0700
                          (float) $values[20]);                             // Balance
 }
-if (!$csvHistory)                                                          exit("Empty CSV history file - nothing to do.\n");
+if (!$csvHistory) exit("Empty CSV history file - nothing to do.\n");
 
 
 // MT4-HistoryFile öffnen bzw. neu anlegen
