5508752fc4c4e457e8e173e7a4f47cbbd785d59c
Peter Walther <pewa-github@rosasurfer.com>
2014-09-06 03:47:40 +0000

    *** empty log message ***

--- src/WEB-INF/bin/simpletrader/syncSignals.php
+++ src/WEB-INF/bin/simpletrader/syncSignals.php
@@ -8,12 +8,49 @@
 require(dirName(__FILE__).'/../config.php');
 
 
-// -- Start ----------------------------------------------------------------------------------------------------------------------------------------
+// --- Start --------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-// Befehlszeilenparameter holen
+// Namen aller zur Zeit unterstützten Signale
+$knownSignals = array('dayfox', 'smarttrader', 'smartscalper');
+
+
+// Befehlszeilenparameter einlesen und validieren
 $args = array_slice($_SERVER['argv'], 1);
-if (!$args) {
-   exit("\n  Syntax: ".baseName($_SERVER['PHP_SELF'])."  [dayfox|smartscalper]\n");
+if (sizeOf($args) != 1)                             exit(1|help());
+if (!in_array(strToLower($args[0]), $knownSignals)) exit(1|help('Unknown signal: '.$args[0]));
+
+
+// Signal verarbeiten
+processSignal($args[0]);
+exit(0);
+
+
+// --- Funktionen ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+/**
+ *
+ * @param  string $signal - Signal-Name
+ */
+function processSignal($signal) {
+   // Parametervalidierung
+   if (!is_string($signal)) throw new IllegalTypeException('Illegal type of parameter $signal: '.getType($signal));
+   $signal = strToLower($signal);
+
+   echoPre('syncing signal '.$signal.'...');
+}
+
+
+/**
+ * Hilfefunktion: Zeigt die Syntax des Aufrufs an.
+ *
+ * @param  string $message - zusätzlich zur Syntax anzuzeigende Message
+ */
+function help($message=null) {
+   if (!is_null($message))
+      echo($message."\n");
+   global $knownSignals;
+   echo("\n  Syntax: ".baseName($_SERVER['PHP_SELF'])."  [".implode('|', $knownSignals)."]\n");
 }
 ?>
