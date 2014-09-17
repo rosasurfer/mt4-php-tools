c63406df50d011757eba72620b166fcc488c48d9
Peter Walther <pewa-github@rosasurfer.com>
2014-09-17 23:57:15 +0000

    *** empty log message ***

--- etc/db/myfx_schema.sql
+++ etc/db/myfx_schema.sql
@@ -19,68 +19,70 @@ create database myfx character set latin1;
 use myfx;
 
 
-create table t_account (
+create table t_signal (
    id int unsigned not null auto_increment,
    version timestamp not null default current_timestamp on update current_timestamp,
-   created datetime,
-   company varchar(100) not null,
+   created datetime not null,
+   refid int not null,
+   alias varchar(20) not null,
    name varchar(100) not null,
-   number varchar(50) not null comment 'Kontonummer',
-   currency enum('AUD','CAD','CHF','EUR','GBP','JPY','NZD','USD') not null comment 'Kontowährung',
-   primary key (id),
-   unique key u_company_number (company,number)
+   currency enum('AUD','CAD','CHF','EUR','GBP','JPY','NZD','USD') not null,
+   unique index u_refid (refid),
+   unique index u_alias (alias),
+   unique index u_name (name),
+   primary key (id)
 ) engine = InnoDB;
 
 
 create table t_openposition (
    id int unsigned not null auto_increment,
    version timestamp not null default current_timestamp on update current_timestamp,
-   created datetime,
+   created datetime not null,
    ticket varchar(50) not null,
    type enum('buy','sell') not null comment 'buy | sell',
-   units int unsigned not null comment 'traded units (not lots)',
-   symbol char(11),
-   opentime datetime not null comment 'timezone: FXT',
+   lots decimal(10,2) unsigned not null,
+   symbol char(11) not null,
+   opentime datetime not null comment 'FXT',
    openprice decimal(10,5) unsigned not null,
    commission decimal(10,2) not null default 0.00,
    swap decimal(10,2) not null default 0.00,
    magicnumber int unsigned,
    comment varchar(255) not null default '',
-   account_id int unsigned not null,
+   signal_id int unsigned not null,
    primary key (id),
-   unique key u_account_id_ticket (account_id,ticket),
+   unique key u_signal_id_ticket (signal_id,ticket),
    index i_opentime_ticket (opentime,ticket),
-   index i_account_id (account_id),
-   constraint openposition_account_id foreign key (account_id) references t_account (id) on delete restrict on update cascade
+   index i_signal_id (signal_id),
+   constraint openposition_signal_id foreign key (signal_id) references t_signal (id) on delete restrict on update cascade
 ) engine = InnoDB;
 
 
 create table t_closedposition (
    id int unsigned not null auto_increment,
    version timestamp not null default current_timestamp on update current_timestamp,
-   created datetime,
+   created datetime not null,
    ticket varchar(50) not null,
    type enum('buy','sell') not null comment 'buy | sell',
-   units int unsigned not null comment 'traded units (not lots)',
-   symbol char(11),
-   opentime datetime not null comment 'timezone: FXT',
+   lots decimal(10,2) unsigned not null,
+   symbol char(11) not null,
+   opentime datetime not null comment 'FXT',
    openprice decimal(10,5) unsigned not null,
-   closetime datetime not null comment 'timezone: FXT',
+   closetime datetime not null comment 'FXT',
    closeprice decimal(10,5) unsigned not null,
    commission decimal(10,2) not null default 0.00,
    swap decimal(10,2) not null default 0.00,
    profit decimal(10,2) not null default 0.00,
    magicnumber int unsigned,
    comment varchar(255) not null default '',
-   account_id int unsigned not null,
+   signal_id int unsigned not null,
    primary key (id),
-   unique key u_account_id_ticket (account_id,ticket),
+   unique key u_signal_id_ticket (signal_id,ticket),
    index i_opentime (opentime),
    index i_closetime (closetime),
    index i_opentime_closetime (opentime,closetime),
    index i_closetime_opentime_ticket (closetime,opentime,ticket),
-   index i_account_id (account_id),
-   constraint closedposition_account_id foreign key (account_id) references t_account (id) on delete restrict on update cascade
+   index i_signal_id (signal_id),
+   constraint closedposition_signal_id foreign key (signal_id) references t_signal (id) on delete restrict on update cascade
 ) engine = InnoDB;
 
 
