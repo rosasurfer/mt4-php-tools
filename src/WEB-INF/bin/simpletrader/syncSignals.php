f152e53c39e5b2aedbc842a8f9fcaaa228020407
Peter Walther <pewa-github@rosasurfer.com>
2014-09-18 23:58:31 +0000

    *** empty log message ***

--- src/WEB-INF/bin/simpletrader/syncSignals.php
+++ src/WEB-INF/bin/simpletrader/syncSignals.php
@@ -139,7 +139,7 @@ function processSignal($signal) {
  * @param  array  $currentHistory       - Array mit aktuellen Historydaten
  */
 function updateTrades($signal, array &$currentOpenPositions, array &$currentHistory) {
-   $unchangedPositions = 0;
+   $unchangedPositions = $changedPositions = 0;
 
    // (1) letzten bekannten Stand der offenen Positionen holen
    $knownOpenPositions = OpenPosition ::dao()->listBySignalAlias($signal, $assocTicket=true);
@@ -163,6 +163,7 @@ function updateTrades($signal, array &$currentOpenPositions, array &$currentHist
                                                      ->setTakeProfit($data['takeprofit'])
                                                      ->save();
             echoPre('modified position: '.ucFirst($position->getType()).' '.$position->getLots().' lot '.$position->getSymbol().' @ '.$position->getOpenPrice().$slMsg.$tpMsg);
+            $changedPositions++;
          }
          else $unchangedPositions++;
          unset($knownOpenPositions[$sTicket]);              // geprüfte Position aus Liste löschen
@@ -175,12 +176,12 @@ function updateTrades($signal, array &$currentOpenPositions, array &$currentHist
    $closedPositions    = $knownOpenPositions;               // alle in $knownOpenPositions übrig gebliebenen Positionen müssen geschlossen worden sein
    $hstSize            = sizeOf($currentHistory);
    $matchingHstEntries = $newHstEntries = 0;                // nach 3 übereinstimmenden Historyeinträgen wird das Update abgebrochen
+   $position           = null;
 
    for ($i=$hstSize-1; $i >= 0; $i--) {                     // History wird rückwärts verarbeitet und bricht bei Übereinstimmung der Daten ab (schnellste Variante)
-      $data     = $currentHistory[$i];
-      $ticket   = $data['ticket'];
-      $position = null;
-      $wasOpen  = false;
+      $data    = $currentHistory[$i];
+      $ticket  = $data['ticket'];
+      $wasOpen = false;
 
       if ($closedPositions) {
          $sTicket = (string) $ticket;
@@ -209,7 +210,8 @@ function updateTrades($signal, array &$currentOpenPositions, array &$currentHist
    $newHstEntries && echoPre($newHstEntries.' new history entr'.($newHstEntries==1 ? 'y':'ies'));
 
    if ($closedPositions) throw new plRuntimeException('Found '.sizeOf($closedPositions)." orphaned open positions:\n".printFormatted($closedPositions, true));
-   echoPre('done');
+
+   !$unchangedPositions && !$changedPositions && !$position && !$newHstEntries && echoPre('done');
 }
 
 
