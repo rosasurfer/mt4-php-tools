b64b34b0c65b1fb7fc178cb9452d57393997d3b9
Peter Walther <pewa-github@rosasurfer.com>
2014-09-27 14:33:41 +0000

    *** empty log message ***

--- src/WEB-INF/config.php
+++ src/WEB-INF/config.php
@@ -2,10 +2,10 @@
 define('APPLICATION_NAME', 'myfx.pewasoft');                               // APPLICATION_ROOT: {project}/src
 define('APPLICATION_ROOT', join(DIRECTORY_SEPARATOR, array_slice(explode(DIRECTORY_SEPARATOR, dirName(__FILE__)), 0, -1)));
 
-ini_set('include_path',      realPath(APPLICATION_ROOT.'/WEB-INF'));       // WEB-INF-Verzeichnis einbinden
-ini_set('session.save_path', realPath(APPLICATION_ROOT.'/../etc/tmp'));
-ini_set('apd.dumpdir',       realPath(APPLICATION_ROOT.'/../etc/tmp'));
-ini_set('error_log',         realPath(APPLICATION_ROOT.'/../etc/log/php_error_log'));
+ini_set('include_path',      APPLICATION_ROOT.'/WEB-INF');                 // WEB-INF-Verzeichnis einbinden
+ini_set('session.save_path', APPLICATION_ROOT.'/../etc/tmp');
+ini_set('apd.dumpdir',       APPLICATION_ROOT.'/../etc/tmp');
+ini_set('error_log',         APPLICATION_ROOT.'/../etc/log/php_error_log');
 
 require(APPLICATION_ROOT.'/../../php-lib/src/phpLib.php');                 // PHPLib laden
 include(APPLICATION_ROOT.'/WEB-INF/include/defines.php');                  // zusätzliche Definitionen laden
