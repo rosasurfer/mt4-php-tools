5bc816fe7e76b7ab6e974197eba2514decc526d2
Peter Walther <pewa-github@rosasurfer.com>
2014-09-30 12:46:41 +0000

    *** empty log message ***

--- etc/db/myfx_data.sql
+++ etc/db/myfx_data.sql
@@ -6,6 +6,7 @@ insert into t_signal (created, name, alias, referenceid, currency) values
    (now(), 'Caesar2'     , 'caesar2'     , '1619', 'USD'),
    (now(), 'Caesar2.1'   , 'caesar21'    , '1803', 'USD'),
    (now(), 'DayFox'      , 'dayfox'      , '2465', 'EUR'),
+   (now(), 'FX Viper'    , 'fxviper'     , '633' , 'USD'),
    (now(), 'GoldStar'    , 'goldstar'    , '2622', 'USD'),
    (now(), 'SmartScalper', 'smartscalper', '1086', 'USD'),
    (now(), 'SmartTrader' , 'smarttrader' , '1081', 'USD');
@@ -15,6 +16,7 @@ insert into t_signal (created, name, alias, referenceid, currency) values
    set @signal_caesar2      = (select id from t_signal where alias = 'caesar2'     );
    set @signal_caesar21     = (select id from t_signal where alias = 'caesar21'    );
    set @signal_dayfox       = (select id from t_signal where alias = 'dayfox'      );
+   set @signal_fxviper      = (select id from t_signal where alias = 'fxviper'     );
    set @signal_goldstar     = (select id from t_signal where alias = 'goldstar'    );
    set @signal_smarttrader  = (select id from t_signal where alias = 'smarttrader' );
    set @signal_smartscalper = (select id from t_signal where alias = 'smartscalper');
