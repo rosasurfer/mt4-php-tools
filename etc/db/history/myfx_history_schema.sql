88ebbdff77cacffbcd1e785b631cb5aa1472b28f
Peter Walther <pewa-github@rosasurfer.com>
2015-07-26 06:00:31 +0000

    *** empty log message ***

---
+++ etc/db/history/myfx_history_schema.sql
@@ -0,0 +1,12 @@
+/*
+Created     26.07.2015
+Modified    26.07.2015
+Project     MyFX Quote History
+Model       main
+Company
+Author      Peter Walther
+Version     0.1
+Database    MySQL 5
+*/
+
+
