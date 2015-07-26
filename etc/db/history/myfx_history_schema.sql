b991091782b0de7608f587122c69e5c1897210ce
Peter Walther <pewa-github@rosasurfer.com>
2015-07-26 12:40:56 +0000

    *** empty log message ***

--- etc/db/history/myfx_history_schema.sql
+++ etc/db/history/myfx_history_schema.sql
@@ -11,12 +11,28 @@ Database    MySQL 5
 
 
 set sql_mode             = 'traditional';
-set collation_connection = 'latin1_german1_ci';
 set autocommit           = 0;
+set collation_connection = 'latin1_general_ci';
 
 drop database if exists myfx_history;
-create database myfx_history character set latin1;
+create database myfx_history character set latin1 collate latin1_general_ci;
 use myfx_history;
+
+
+create table t_instrument (
+   id int unsigned not null auto_increment,
+   version timestamp not null default current_timestamp on update current_timestamp,
+   created datetime not null,
+   symbol varchar(11) not null,
+   unique index u_symbol (symbol),
+   primary key (id)
+) engine = InnoDB;
+
+
+-- Trigger definitions
+delimiter //
+
+delimiter ;
 
 
 -- Daten einlesen
