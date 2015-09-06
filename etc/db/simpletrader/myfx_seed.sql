7cba4b5ae8caaea26a56d3296a9906b78ab63f68
Peter Walther <pewa-github@rosasurfer.com>
2015-09-06 11:18:57 +0000

    added SimpleTrader signal "TwilightScalper"

--- etc/db/simpletrader/myfx_seed.sql
+++ etc/db/simpletrader/myfx_seed.sql
@@ -2,21 +2,21 @@
 -- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Signals
 insert into t_signal (created, name, alias, referenceid, currency) values
-   (now(), 'AlexProfit'    , 'alexprofit'   , '2474', 'USD'),
-   (now(), 'Asta'          , 'asta'         , '2370', 'GBP'),
-   (now(), 'Caesar2'       , 'caesar2'      , '1619', 'USD'),
-   (now(), 'Caesar2.1'     , 'caesar21'     , '1803', 'USD'),
-   (now(), 'DayFox'        , 'dayfox'       , '2465', 'EUR'),
-   (now(), 'FX Viper'      , 'fxviper'      , '633' , 'USD'),
-   (now(), 'GC-Edge'       , 'gcedge'       , '998' , 'USD'),
-   (now(), 'GoldStar'      , 'goldstar'     , '2622', 'USD'),
-   (now(), 'Kilimanjaro'   , 'kilimanjaro'  , '2905', 'USD'),
-   (now(), 'OverTrader'    , 'overtrader'   , '2973', 'USD'),
-   (now(), 'SmartScalper'  , 'smartscalper' , '1086', 'USD'),
-   (now(), 'SmartTrader'   , 'smarttrader'  , '1081', 'USD'),
-   (now(), 'Steady Capture', 'steadycapture', '4023', 'USD'),
-   (now(), 'Yen Fortress'  , 'yenfortress'  , '2877', 'USD');
-
+   (now(), 'AlexProfit'     , 'alexprofit'   , '2474', 'USD'),
+   (now(), 'Asta'           , 'asta'         , '2370', 'GBP'),
+   (now(), 'Caesar2'        , 'caesar2'      , '1619', 'USD'),
+   (now(), 'Caesar2.1'      , 'caesar21'     , '1803', 'USD'),
+   (now(), 'DayFox'         , 'dayfox'       , '2465', 'EUR'),
+   (now(), 'FX Viper'       , 'fxviper'      , '633' , 'USD'),
+   (now(), 'GC-Edge'        , 'gcedge'       , '998' , 'USD'),
+   (now(), 'GoldStar'       , 'goldstar'     , '2622', 'USD'),
+   (now(), 'Kilimanjaro'    , 'kilimanjaro'  , '2905', 'USD'),
+   (now(), 'OverTrader'     , 'overtrader'   , '2973', 'USD'),
+   (now(), 'SmartScalper'   , 'smartscalper' , '1086', 'USD'),
+   (now(), 'SmartTrader'    , 'smarttrader'  , '1081', 'USD'),
+   (now(), 'Steady Capture' , 'steadycapture', '4023', 'USD'),
+   (now(), 'TwilightScalper', 'twilight'     , '3913', 'USD'),
+   (now(), 'Yen Fortress'   , 'yenfortress'  , '2877', 'USD');
 
    set @signal_alexprofit    = (select id from t_signal where alias = 'alexprofit'   );
    set @signal_asta          = (select id from t_signal where alias = 'asta'         );
@@ -30,6 +30,7 @@ insert into t_signal (created, name, alias, referenceid, currency) values
    set @signal_overtrader    = (select id from t_signal where alias = 'overtrader'   );
    set @signal_smarttrader   = (select id from t_signal where alias = 'smarttrader'  );
    set @signal_steadycapture = (select id from t_signal where alias = 'steadycapture');
+   set @signal_twilight      = (select id from t_signal where alias = 'twilight'     );
    set @signal_yenfortress   = (select id from t_signal where alias = 'yenfortress'  );
 
 
--- etc/db/updates/update_2015-09-06.sql
+++ etc/db/updates/update_2015-09-06.sql
@@ -9,9 +9,13 @@ set collation_connection = 'latin1_german1_ci';
 -- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Signals
 insert into t_signal (created, name, alias, referenceid, currency) values
-   (now(), 'Kilimanjaro'   , 'kilimanjaro'  , '2905', 'USD'),
-   (now(), 'Steady Capture', 'steadycapture', '4023', 'USD')
-   on duplicate key update version = version;
+   (now(), 'Kilimanjaro'    , 'kilimanjaro'  , '2905', 'USD'),
+   (now(), 'Steady Capture' , 'steadycapture', '4023', 'USD'),
+   (now(), 'TwilightScalper', 'twilight'     , '3913', 'USD')
+   on duplicate key update name        = values(name),
+                           alias       = values(alias),
+                           referenceid = values(referenceid),
+                           currency    = values(currency);
 
 -- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
