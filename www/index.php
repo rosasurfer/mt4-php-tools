0c6590ab9d51234e9c7b92b26afb775188ce5f48
Peter Walther <pewa-github@rosasurfer.com>
2016-07-26 21:20:42 +0300

    adjust directory structure to new Ministruts framework

--- www/index.php
+++ www/index.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Zentraler HTTP-Request-Handler
- */
-require(__DIR__.'/../app/init.php');
+if (PHP_VERSION < '5.6') {
+   echo('application error'.PHP_EOL);
+   error_log('Error: A PHP version >= 5.6 is required (found version '.PHP_VERSION.').');
+   exit(1);
+}
+!defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirname(__DIR__));
+!defined('APPLICATION_ID'  ) && define('APPLICATION_ID'  , 'myfx');
 
+
+// web application specific settings
+ini_set('session.save_path', APPLICATION_ROOT.'/etc/tmp');
+
+
+// init web app
+require(APPLICATION_ROOT.'/app/init.php');
+
+
+// run web app
 StrutsController::processRequest();
