f0a6ffbff85c9bc7a0cee989ed8d53cc5185ff20
Peter Walther <pewa-github@rosasurfer.com>
2016-07-26 20:17:30 +0300

    replace cvs with git update script

---
+++ bin/vcs-update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+
+
+# (1) change working directory
+SCRIPT_NAME=$(readlink -e "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_NAME")
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
+cd "$PROJECT_DIR"
+
+
+# (2) update project
+[ ! -d ".git" ] && echo error: .git directory not found in project "$PROJECT_DIR" && exit
+echo Updating $(basename "$PROJECT_DIR")...
+
+git fetch origin                                                                  || exit
+git status                                                                        || exit
+git diff --stat --ignore-space-at-eol HEAD                                        || exit
+git reset --hard origin/master                                                    || exit
+git status                                                                        || exit
+
+
+# (3) check/update additional requirements: dependencies, submodules, file permissions
