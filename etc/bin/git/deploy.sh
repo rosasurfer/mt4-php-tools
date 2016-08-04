c9cbaa2d4ad55e3ea8742170d05991b2c9549279
Peter Walther <pewa-github@rosasurfer.com>
2016-08-05 00:36:49 +0300

    add git pre-commit hook and deploy script

---
+++ etc/bin/git/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+
+# (1) change working directory
+SCRIPT=$(readlink -e "$0")
+DIR=$(dirname "$SCRIPT")
+
+while [ 1 ]; do
+   [ -d "$DIR/.git" ] && break
+   [ "$DIR" = "/"   ] && echo "error: .git directory not found" && exit 1
+   DIR=$(dirname "$DIR")
+done
+
+cd "$DIR"
+
+
+# (2) update project
+echo Updating "$DIR"...
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git fetch origin                                                                  || exit
+git --no-pager status                                                             || exit
+git --no-pager diff --stat --ignore-space-at-eol HEAD origin/$BRANCH              || exit
+git reset --hard origin/$BRANCH                                                   || exit
+git --no-pager status                                                             || exit
+
+
+# (3) check/update additional requirements: dependencies, submodules, file permissions
---
+++ etc/bin/git/pre-commit.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# A hook script called by "git commit" without any arguments. The hook should
+# exit with non-zero status after issuing an appropriate message if it wants
+# to stop the commit.
+#
+# Link this script to ".git/hooks/pre-commit" if it's located elsewhere.
+#
+
+
+# redirect output to stderr
+exec 1>&2
+
+
+# check for and execute a user hook
+if [ -f "$0.user" ]; then
+   "$0.user" "$@" || exit 1
+fi
+
+
+# get the files to be committed
+IFS=$'\n'
+for file in $(git diff --staged --name-only); do
+   # skip files to delete and error out on found BOM headers
+   [ -f "$file" ] && file "$file" | grep 'UTF-8 Unicode (with BOM)' && exit 1
+done
+
+exit 0
