725b8f1d960bdb0b532ca678a9e7b70f2c317754
Peter Walther <pewa-github@rosasurfer.com>
2016-08-24 11:20:34 +0300

    update db backup script

--- etc/db/backup/backup.sh
+++ etc/db/backup/backup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-mysqldump -uroot --add-drop-database --allow-keywords --triggers --routines -iclq -r signals.sql -B myfx
+DATE=$(date '+%Y.%m.%d_%H.%M')
+
+mysqldump="$(which mysqldump 2>&-)"
+
+if [ -z "$mysqldump" ]; then
+   echo mysqldump not found
+   exit
+fi
+
+$mysqldump -uroot --add-drop-database --allow-keywords --triggers --routines -iclq -r signals_${DATE}.sql -B myfx
