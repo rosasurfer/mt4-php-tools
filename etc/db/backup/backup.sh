c7b156501e14dc88671f74dc2b87f068dff2afba
Peter Walther <pewa-github@rosasurfer.com>
2016-09-08 15:05:41 +0300

    replace external with built-in command

--- etc/db/backup/backup.sh
+++ etc/db/backup/backup.sh
@@ -2,11 +2,9 @@
 
 DATE=$(date '+%Y.%m.%d_%H.%M')
 
-mysqldump="$(which mysqldump 2>&-)"
-
-if [ -z "$mysqldump" ]; then
+if ! command -v 'mysqldump' >/dev/null 2>&1; then
    echo mysqldump not found
    exit
 fi
 
-$mysqldump -uroot --add-drop-database --allow-keywords --triggers --routines -iclq -r signals_${DATE}.sql -B myfx
+mysqldump -uroot --add-drop-database --allow-keywords --triggers --routines -iclq -r signals_${DATE}.sql -B myfx
