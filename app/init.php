27211bf4533fef77ce8365da5ed3a266cf32abff
Peter Walther <pewa-github@rosasurfer.com>
2016-09-13 03:36:37 +0300

    rename "app/defines.php" to "app/definitions.php"

--- app/init.php
+++ app/init.php
@@ -6,26 +6,27 @@ if (PHP_VERSION < '5.6') {
 }
 
 
-// check application constants
+// check/update app configuration
 !defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirname(__DIR__));
 !defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'myfx');
 
-
-// global settings
 error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set('error_log', APPLICATION_ROOT.'/etc/log/php_error.log');
 !isSet($_SERVER['REQUEST_METHOD']) && set_time_limit(0);          // no time limit for CLI
 
 
-// load Ministruts and project definitions
+// load MVC framework
 require(APPLICATION_ROOT.'/etc/vendor/rosasurfer/ministruts/src/load-global.php');
-include(APPLICATION_ROOT.'/app/defines.php');
 
 
-// TODO: replace with case-insensitive loader
+// register class loader                                          TODO: replace with case-insensitive loader
 use phalcon\Loader as ClassLoader;
 (new ClassLoader())->registerClasses(include(__DIR__.'/classmap.php'))
                    ->register();
+
+// load project definitions
+include(APPLICATION_ROOT.'/app/definitions.php');
+
 
 
 /**
