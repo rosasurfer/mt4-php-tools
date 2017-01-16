8f51ee3e6e2035144901e7d813a36427660fa68b
Peter Walther <pewa-github@rosasurfer.com>
2017-01-16 02:54:13 +0200

    white space

--- etc/db/users.sql
+++ etc/db/users.sql
@@ -1,13 +1,9 @@
-
 use mysql;
 
 grant usage on * to 'myfx'@'localhost',
                     'myfx'@'mt4.rosasurfer.com';
-
 revoke all privileges, grant option from 'myfx'@'localhost',
                                          'myfx'@'mt4.rosasurfer.com';
-
 grant select, insert, update, delete, lock tables, create, execute, drop, create temporary tables on `myfx`.* to 'myfx'@'localhost'            identified by 'passwd',
                                                                                                                  'myfx'@'mt4.rosasurfer.com' identified by 'passwd';
-
 flush privileges;
