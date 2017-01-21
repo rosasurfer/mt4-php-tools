1323b9b83f23fec047954cf54fd6f9c2d954fa9d
Peter Walther <pewa-github@rosasurfer.com>
2017-01-21 18:43:59 +0200

    update application root url

--- web/index.php
+++ web/index.php
@@ -1,13 +1,10 @@
 <?php
 use rosasurfer\ministruts\StrutsController;
 
-// configure and init web app
+// configure and init the web app
 !defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
 !defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'myfx');
-
-PHP::ini_set('session.save_path', APPLICATION_ROOT.'/etc/tmp');
 require(APPLICATION_ROOT.'/app/init.php');
 
-
-// run web app
+// run the web app
 StrutsController::processRequest();
