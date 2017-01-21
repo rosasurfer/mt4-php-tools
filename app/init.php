9af545b67fc6a6cfcc8e642b5fd6097aaaa67411
Peter Walther <pewa-github@rosasurfer.com>
2017-01-21 19:10:47 +0200

    replace Phalcon class loader with Composer

--- app/init.php
+++ app/init.php
@@ -1,24 +1,23 @@
 <?php
-use phalcon\Loader as ClassLoader;
+if (PHP_VERSION_ID < 50600) exit('[FATAL]  This application requires PHP >= 5.6');
 
 use rosasurfer\MiniStruts;
 use rosasurfer\util\PHP;
 
 
-// define application root and project helpers
+// check app configuration
 !defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
 !defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'myfx');
-include(APPLICATION_ROOT.'/app/definitions.php');
 
 
-// register MiniStruts and Phalcon class loader
-require(APPLICATION_ROOT.'/etc/vendor/rosasurfer/ministruts/src/load.php');
-(new ClassLoader())->registerClasses(include(__DIR__.'/classmap.php'))->register();
+// load Composer
+require(APPLICATION_ROOT.'/etc/vendor/autoload.php');
 
 
-// error logging
+// global settings
 error_reporting(E_ALL & ~E_DEPRECATED);
-PHP::ini_set('error_log', APPLICATION_ROOT.'/etc/log/php_error.log');
+PHP::ini_set('error_log'      , APPLICATION_ROOT.'/etc/log/php-error.log');
+PHP::ini_set('default_charset', 'UTF-8'                                  );
 
 
 // initialize MiniStruts
