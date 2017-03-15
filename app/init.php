efc1e23210b0ebed46fc9515db02813790639001
Peter Walther <pewa-github@rosasurfer.com>
2017-03-15 14:57:18 +0200

    use new AutoConfiguration

--- app/init.php
+++ app/init.php
@@ -1,23 +1,16 @@
 <?php
 use rosasurfer\MiniStruts;
-use rosasurfer\util\PHP;
 
+// global settings
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('error_log'      , __DIR__.'/../etc/log/php-error.log');
+ini_set('default_charset', 'UTF-8'                            );
+!defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
 
 // class loader
 require(__DIR__.'/../etc/vendor/autoload.php');
 
-
-// check app configuration
-!defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
-
-
-// global settings
-error_reporting(E_ALL & ~E_DEPRECATED);
-PHP::ini_set('error_log'      , __DIR__.'/../etc/log/php-error.log');
-PHP::ini_set('default_charset', 'UTF-8'                            );
-
-
-// initialize MiniStruts
+// initialize application
 MiniStruts::init([
    'config'  => __DIR__.'/config',
    'globals' => true,
