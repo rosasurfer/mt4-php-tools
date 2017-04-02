f3afca26e347dd3974cc6048cde19e644fbd3dc2
Peter Walther <pewa-github@rosasurfer.com>
2017-04-02 21:14:28 +0300

    update PHPStan configuration

--- etc/phpstan/autoload.php
+++ etc/phpstan/autoload.php
@@ -1,3 +1,6 @@
-<?php
+<?php declare(strict_types=1);
+
 include(__DIR__.'/../vendor/autoload.php');
 include(__DIR__.'/../vendor/rosasurfer/ministruts/src/globals.php');
+include(__DIR__.'/../vendor/rosasurfer/ministruts/etc/phpstan/PersistableObjectDaoReturnType.php');
+include(__DIR__.'/../vendor/rosasurfer/ministruts/etc/phpstan/SingletonGetInstanceReturnType.php');
--- etc/phpstan/function-stubs.php
+++ etc/phpstan/function-stubs.php
@@ -1,4 +1,5 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace {
 
     if (!function_exists('pcntl_signal')) {
