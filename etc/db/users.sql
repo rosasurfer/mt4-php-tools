8f1c0178355b9ce706f2de7593250562fe63a05b
Peter Walther <pewa-github@rosasurfer.com>
2017-04-10 13:52:50 +0300

    remove lagecy database schema

--- etc/db/users.sql
+++ etc/db/users.sql
@@ -4,6 +4,6 @@ grant usage on * to 'myfx'@'localhost',
                     'myfx'@'mt4.rosasurfer.com';
 revoke all privileges, grant option from 'myfx'@'localhost',
                                          'myfx'@'mt4.rosasurfer.com';
-grant select, insert, update, delete, lock tables, create, execute, drop, create temporary tables on `myfx`.* to 'myfx'@'localhost'            identified by 'passwd',
+grant select, insert, update, delete, lock tables, create, execute, drop, create temporary tables on `myfx`.* to 'myfx'@'localhost'          identified by 'passwd',
                                                                                                                  'myfx'@'mt4.rosasurfer.com' identified by 'passwd';
 flush privileges;
