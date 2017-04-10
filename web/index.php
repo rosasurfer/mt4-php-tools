82e376c85cda0c9dcbc44c651c53c8ccdad476da
Peter Walther <pewa-github@rosasurfer.com>
2017-04-10 12:50:58 +0300

    remove global definition of APPLICATION_ID

--- web/index.php
+++ web/index.php
@@ -1,11 +1,8 @@
 <?php
-if (PHP_VERSION_ID < 50600) exit('[FATAL] This application requires PHP >= 5.6');
-
 use rosasurfer\ministruts\StrutsController;
 
 // configure and init the web app
 !defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
-!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'myfx');
 require(APPLICATION_ROOT.'/app/init.php');
 
 // run the web app
