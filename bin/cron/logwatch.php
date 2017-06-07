26e5bdfdf3ef7d922cd4f57c45599c8e28482fc2
Peter Walther <pewa-github@rosasurfer.com>
2017-06-08 01:28:02 +0300

    update logwatch script

--- bin/cron/logwatch.php
+++ bin/cron/logwatch.php
@@ -3,32 +3,25 @@
 namespace rosasurfer\xtrade\logwatch;
 
 /**
- * Scans the application's PHP error log file for entries and notifies by email. Mail will be sent to the configured log
- * message receivers. If no receivers are configured mail will be sent to the system user running the script. Log entries
- * notifications have been sent for are removed from the log file.
+ * Scans the application's PHP error log file for entries and notifies by email. Mail is sent to the configured log message
+ * receivers. If no receivers are configured mail is sent to the system user running the script. Processed log entries are
+ * removed from the log file.
  *
  * TODO: Error messages must not be printed to STDOUT but to STDERR.
- * TODO: Parameter for (not) suppressing regular output to enable status messages when not executed by CRON.
+ * TODO: Add parameter for not suppressing regular output to get status messages when not executed by CRON.
  */
 use rosasurfer\config\Config;
 use rosasurfer\exception\IllegalTypeException;
 use rosasurfer\util\PHP;
 
-use function rosasurfer\echoPre;
-use function rosasurfer\strStartsWith;
-
-use const rosasurfer\CLI;
-use const rosasurfer\NL;
-use const rosasurfer\WINDOWS;
-
-require(__DIR__.'/../app/init.php');
+require(__DIR__.'/../../app/init.php');
 set_time_limit(0);                                       // no time limit for CLI
 
 
 // --- configuration --------------------------------------------------------------------------------------------------------
 
 
-$quiet = false;                                          // whether or not "quiet" mode is enabled (for cron)
+$quiet = false;                                          // whether or not "quiet" mode is enabled (for CRON)
 
 
 // --- parse/validate command line arguments --------------------------------------------------------------------------------
@@ -56,7 +49,7 @@ $sender = $config->get('mail.from', get_current_user().'@localhost');
 $receivers = [];
 foreach (explode(',', $config->get('log.mail.receiver', '')) as $receiver) {
     if ($receiver=trim($receiver))
-        $receivers[] = $receiver;                          // @TODO: validate address format
+        $receivers[] = $receiver;       // TODO: validate address format
 }
 
 // check setting "mail.forced-receiver" (may be set in development)
@@ -68,7 +61,7 @@ if ($receivers && $forcedReceivers=$config->get('mail.forced-receiver', false)) 
     }
 }
 
-// w/o receiver mail is sent to the current system user
+// without receiver mail is sent to the current system user
 !$receivers && $receivers[]=get_current_user().'@localhost';
 
 
@@ -86,7 +79,7 @@ if (!is_file    ($errorLog)) { $quiet || echoPre('error log does not exist: '.$e
 if (!is_writable($errorLog)) {             error('cannot access log file: '  .$errorLog); exit(1); }
 $errorLog = realPath($errorLog);
 
-// rename the file (we don't want to lock it, doing so could block the main app)
+// rename the file; we don't want to lock it cause doing so could block the main app
 $tempName = tempNam(dirName($errorLog), baseName($errorLog).'.');
 if (!rename($errorLog, $tempName)) {
     error('cannot rename log file: '  .$errorLog);
@@ -100,20 +93,20 @@ $line  = $entry = '';
 $i = 0;
 while (($line=fGets($hFile)) !== false) {
     $i++;
-    if (strStartsWith($line, '[')) {                      // lines starting with "[" are considered the start of an entry
+    if (strStartsWith($line, '[')) {            // lines starting with a bracket "[" are considered the start of an entry
         processEntry($entry);
         $entry = '';
     }
     $entry .= $line;
 }
-processEntry($entry);                                    // process the last entry (if any)
+processEntry($entry);                           // process the last entry (if any)
 
 // delete the processed file
 fClose($hFile);
 unlink($tempName);
 
 
-// (4) The ugly end
+// (4) the ugly end
 exit(0);
 
 
@@ -129,17 +122,14 @@ exit(0);
 function processEntry($entry) {
     if (!is_string($entry)) throw new IllegalTypeException('Illegal type of parameter $entry: '.getType($entry));
     $entry = trim($entry);
-    if (!strLen($entry))    return;
+    if (!strLen($entry)) return;
 
     global $quiet, $sender, $receivers;
 
-    // normalize line-breaks
-    $entry = str_replace(["\r\n", "\r"], "\n", $entry);            // use Unix line-breaks by default but...
-    if (WINDOWS)                                                   // use Windows line-breaks on Windows
-        $entry = str_replace("\n", "\r\n", $entry);
-    $entry = str_replace(chr(0), '?', $entry);                     // replace NUL bytes which destroy the mail
+    $entry = normalizeEOL($entry, WINDOWS ? EOL_WINDOWS:EOL_UNIX);  // normalize line-breaks for email
+    $entry = str_replace(chr(0), '?', $entry);                      // replace NUL bytes which destroy the mail
 
-    $subject = strTok($entry, "\r\n");                             // that's CR or LF, not CRLF
+    $subject = strTok($entry, "\r\n");                              // that's CR or LF, not CRLF
     $message = $entry;
 
     $quiet || echoPre('sending log entry: '.subStr($subject, 0, 50).'...');
@@ -166,7 +156,7 @@ function error($message) {
 /**
  * Help. Display script syntax.
  *
- * @param  string $message - additional message to display (default: none)
+ * @param  string $message [optional] - additional message to display (default: none)
  */
 function help($message = null) {
     if (!is_null($message))
