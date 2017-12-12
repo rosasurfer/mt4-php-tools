af2b325ae1f603da12ae399144efe3fb76202326
Peter Walther <pewa-github@rosasurfer.com>
2017-12-12 21:23:12 +0200

    add temp directory config

--- app/init.php
+++ app/init.php
@@ -3,20 +3,22 @@ use rosasurfer\Application;
 use rosasurfer\util\PHP;
 
 // class loader
-require(__DIR__.'/../vendor/autoload.php');
+$appRoot = dirName(__DIR__);
+require($appRoot.'/vendor/autoload.php');
 
 
 // php.ini settings
 error_reporting(E_ALL & ~E_DEPRECATED);
-PHP::ini_set('error_log',         __DIR__.'/../etc/log/php-error.log');
-PHP::ini_set('session.save_path', __DIR__.'/../etc/tmp'              );
-PHP::ini_set('default_charset',  'UTF-8'                             );
-PHP::ini_set('memory_limit',     '256M'                              );
+PHP::ini_set('error_log',         $appRoot.'/etc/log/php-error.log');
+PHP::ini_set('session.save_path', $appRoot.'/etc/tmp'              );
+PHP::ini_set('default_charset',  'UTF-8'                           );
+PHP::ini_set('memory_limit',     '256M'                            );
 
 
 // create a new application
 return new Application([
-    'app.dir.root'       => dirName(__DIR__),
+    'app.dir.root'       => $appRoot,
     'app.dir.config'     => __DIR__.'/config',
+    'app.dir.temp'       => $appRoot.'/etc/tmp',
     'app.global-helpers' => true,
 ]);
