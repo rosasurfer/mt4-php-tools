ce53d2a33634542224bf10f0bc6b5e77cfd6ffc7
Peter Walther <pewa-github@rosasurfer.com>
2018-04-01 19:11:29 +0300

    update CLI phpinfo.php

--- bin/phpinfo.php
+++ bin/phpinfo.php
@@ -3,12 +3,9 @@
 /**
  * Command line version of phpInfo()
  */
-use rosasurfer\Application;
 use rosasurfer\util\PHP;
 
 require(dirName(realPath(__FILE__)).'/../app/init.php');
-
-$app = new Application();                               // creating an application loads the configuration
 
 PHP::phpInfo();
 echo PHP_EOL.'loaded php.ini: "'.php_ini_loaded_file().'"'.PHP_EOL;
