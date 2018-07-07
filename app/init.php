47649cf718d2cbac45d242dce525e3b0555db1fb
Peter Walther <pewa-github@rosasurfer.com>
2018-07-07 05:33:24 +0300

    update Apache configuration

--- app/init.php
+++ app/init.php
@@ -9,6 +9,7 @@ require($appRoot.'/vendor/autoload.php');
 
 // php.ini settings
 error_reporting(E_ALL & ~E_DEPRECATED);
+PHP::ini_set('log_errors',        1                                );
 PHP::ini_set('error_log',         $appRoot.'/etc/log/php-error.log');
 PHP::ini_set('session.save_path', $appRoot.'/etc/tmp'              );
 PHP::ini_set('default_charset',  'UTF-8'                           );
@@ -19,6 +20,5 @@ PHP::ini_set('memory_limit',     '256M'                            );
 return new Application([
     'app.dir.root'       => $appRoot,
     'app.dir.config'     => __DIR__.'/config',
-    'app.dir.temp'       => $appRoot.'/etc/tmp',
     'app.global-helpers' => true,
 ]);
