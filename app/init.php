beda612e8ac5433c07727be6393734a9a2d3011d
Peter Walther <pewa-github@rosasurfer.com>
2019-02-09 11:00:35 +0200

    update PHPStan configuration

--- app/init.php
+++ app/init.php
@@ -1,19 +1,17 @@
 <?php
 use rosasurfer\Application;
 use rosasurfer\util\PHP;
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 // class loader
 require(($appRoot=dirname(__DIR__)).'/vendor/autoload.php');
 
-
 // php.ini settings
-error_reporting(E_ALL & ~E_DEPRECATED);
 PHP::ini_set('log_errors',        1                                );
 PHP::ini_set('error_log',         $appRoot.'/etc/log/php-error.log');
 PHP::ini_set('session.save_path', $appRoot.'/etc/tmp'              );
 PHP::ini_set('default_charset',  'UTF-8'                           );
 PHP::ini_set('memory_limit',     '256M'                            );
-
 
 // create a new application
 return new Application([
