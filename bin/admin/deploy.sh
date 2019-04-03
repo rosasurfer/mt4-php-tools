155c36562dbb12848bfc91e1b81f42cdf8f53ecc
Peter Walther <pewa-github@rosasurfer.com>
2019-04-03 22:26:44 +0300

    white space

--- bin/admin/deploy.sh
+++ bin/admin/deploy.sh
@@ -23,14 +23,14 @@ NOTIFY_RECEIVER="${NOTIFY_RECEIVER:-<email@domain.tld>}"        `# fill in the n
 
 # print a message to stderr
 function error() {
-    echo "error: $@" 1>&2
+   echo "error: $@" 1>&2
 }
 
 # --- end of functions ------------------------------------------------------------------------------------------------------
 
 
 # check git availability
-command -v git >/dev/null || { error "ERROR: Git command not found."; exit 1; }
+command -v git > /dev/null || { error "ERROR: Git command not found."; exit 1; }
 
 
 # change to the project's toplevel directory
@@ -47,28 +47,28 @@ git fetch origin
 
 # check arguments
 if [ $# -eq 0 ]; then
-    # no arguments given, get current branch name
-    if [ "$FROM_BRANCH" = "HEAD" ]; then
-        error "HEAD is currently detached at $FROM_COMMIT, you must specify a ref-name to deploy."
-        error "Usage: $(basename "$0") [<branch-name> | <tag-name> | <commit-sha>]"
-        exit 2
-    fi
-    BRANCH="$FROM_BRANCH"
+   # no arguments given, get current branch name
+   if [ "$FROM_BRANCH" = "HEAD" ]; then
+      error "HEAD is currently detached at $FROM_COMMIT, you must specify a ref-name to deploy."
+      error "Usage: $(basename "$0") [<branch-name> | <tag-name> | <commit-sha>]"
+      exit 2
+   fi
+   BRANCH="$FROM_BRANCH"
 elif [ $# -eq 1 ]; then
-    # argument given, resolve its type
-    if git show-ref -q --verify "refs/heads/$1"; then
-        BRANCH="$1"
-    elif git show-ref -q --verify "refs/remotes/origin/$1"; then
-        BRANCH="$1"
-    elif git show-ref -q --verify "refs/tags/$1"; then
-        TAG="$1"
-    elif git rev-parse -q --verify "$1^{commit}" >/dev/null; then
-        COMMIT="$1"
-    else
-        error "Unknown ref-name $1"
-        error "Usage: $(basename "$0") [<branch-name> | <tag-name> | <commit-sha>]"
-        exit 2
-    fi
+   # argument given, resolve its type
+   if git show-ref -q --verify "refs/heads/$1"; then
+      BRANCH="$1"
+   elif git show-ref -q --verify "refs/remotes/origin/$1"; then
+      BRANCH="$1"
+   elif git show-ref -q --verify "refs/tags/$1"; then
+      TAG="$1"
+   elif git rev-parse -q --verify "$1^{commit}" > /dev/null; then
+      COMMIT="$1"
+   else
+      error "Unknown ref-name $1"
+      error "Usage: $(basename "$0") [<branch-name> | <tag-name> | <commit-sha>]"
+      exit 2
+   fi
 else
     error "Usage: $(basename "$0") [<branch-name> | <tag-name> | <commit-sha>]"
     exit 2
@@ -77,12 +77,12 @@ fi
 
 # update project
 if [ -n "$BRANCH" ]; then
-    [ "$BRANCH" != "$FROM_BRANCH" ] && git checkout "$BRANCH"
-    git merge --ff-only "origin/$BRANCH"
+   [ "$BRANCH" != "$FROM_BRANCH" ] && git checkout "$BRANCH"
+   git merge --ff-only "origin/$BRANCH"
 elif [ -n "$TAG"    ]; then
-    git checkout "$TAG"
+   git checkout "$TAG"
 elif [ -n "$COMMIT" ]; then
-    git checkout "$COMMIT"
+   git checkout "$COMMIT"
 fi
 
 
@@ -91,42 +91,42 @@ OLD=$FROM_COMMIT
 NEW=$(git rev-parse --short HEAD)
 
 if [ "$OLD" = "$NEW" ]; then
-    echo No changes.
+   echo No changes.
 else
-    # send deployment notifications if configured
+   # send deployment notifications if configured
 
-    # trim angle brackets
-    NOTIFY_FOR_PROJECT=${NOTIFY_FOR_PROJECT#<}; NOTIFY_FOR_PROJECT=${NOTIFY_FOR_PROJECT%>}
-    NOTIFY_ON_HOST=${NOTIFY_ON_HOST#<};         NOTIFY_ON_HOST=${NOTIFY_ON_HOST%>}
-    NOTIFY_RECEIVER=${NOTIFY_RECEIVER#<};       NOTIFY_RECEIVER=${NOTIFY_RECEIVER%>}
+   # trim angle brackets
+   NOTIFY_FOR_PROJECT=${NOTIFY_FOR_PROJECT#<}; NOTIFY_FOR_PROJECT=${NOTIFY_FOR_PROJECT%>}
+   NOTIFY_ON_HOST=${NOTIFY_ON_HOST#<};         NOTIFY_ON_HOST=${NOTIFY_ON_HOST%>}
+   NOTIFY_RECEIVER=${NOTIFY_RECEIVER#<};       NOTIFY_RECEIVER=${NOTIFY_RECEIVER%>}
 
-    # reset default values
-    [ "$NOTIFY_FOR_PROJECT" = "project-name"     ] && NOTIFY_FOR_PROJECT=
-    [ "$NOTIFY_ON_HOST"     = "hostname"         ] && NOTIFY_ON_HOST=
-    [ "$NOTIFY_RECEIVER"    = "email@domain.tld" ] && NOTIFY_RECEIVER=
+   # reset default values
+   [ "$NOTIFY_FOR_PROJECT" = "project-name"     ] && NOTIFY_FOR_PROJECT=
+   [ "$NOTIFY_ON_HOST"     = "hostname"         ] && NOTIFY_ON_HOST=
+   [ "$NOTIFY_RECEIVER"    = "email@domain.tld" ] && NOTIFY_RECEIVER=
 
-    # check missing values
-    NOTIFY=0
-    if [[ -n "$NOTIFY_ON_HOST" && -n "$NOTIFY_RECEIVER" ]]; then
-        if [ "$NOTIFY_ON_HOST" = "$(hostname)" ]; then
-            [ -z "$NOTIFY_FOR_PROJECT" ] && NOTIFY_FOR_PROJECT=$(basename $(git config --get remote.origin.url) .git)
-            NOTIFY=1
-        fi
-    fi
+   # check missing values
+   NOTIFY=0
+   if [[ -n "$NOTIFY_ON_HOST" && -n "$NOTIFY_RECEIVER" ]]; then
+      if [ "$NOTIFY_ON_HOST" = "$(hostname)" ]; then
+         [ -z "$NOTIFY_FOR_PROJECT" ] && NOTIFY_FOR_PROJECT=$(basename $(git config --get remote.origin.url) .git)
+         NOTIFY=1
+      fi
+   fi
 
-    # notify
-    if [ $NOTIFY -eq 1 ]; then
-        if command -v sendmail >/dev/null; then
-            (
-            echo 'From: "Deployments '$NOTIFY_FOR_PROJECT'" <'$NOTIFY_RECEIVER'>'
-            if   [ -n "$BRANCH" ]; then echo "Subject: Updated $NOTIFY_FOR_PROJECT, branch $BRANCH to latest ($NEW)"
-            elif [ -n "$TAG"    ]; then echo "Subject: Reset $NOTIFY_FOR_PROJECT to tag $TAG ($NEW)"
-            elif [ -n "$COMMIT" ]; then echo "Subject: Reset $NOTIFY_FOR_PROJECT to commit $COMMIT"
-            fi
-            git log --pretty='%h %ae %s' $OLD..$NEW
-            ) | sendmail -f "$NOTIFY_RECEIVER" "$NOTIFY_RECEIVER"
-        fi
-    fi
+   # notify
+   if [ $NOTIFY -eq 1 ]; then
+      if command -v sendmail > /dev/null; then
+         (
+         echo 'From: "Deployments '$NOTIFY_FOR_PROJECT'" <'$NOTIFY_RECEIVER'>'
+         if   [ -n "$BRANCH" ]; then echo "Subject: Updated $NOTIFY_FOR_PROJECT, branch $BRANCH to latest ($NEW)"
+         elif [ -n "$TAG"    ]; then echo "Subject: Reset $NOTIFY_FOR_PROJECT to tag $TAG ($NEW)"
+         elif [ -n "$COMMIT" ]; then echo "Subject: Reset $NOTIFY_FOR_PROJECT to commit $COMMIT"
+         fi
+         git log --pretty='%h %ae %s' $OLD..$NEW
+         ) | sendmail -f "$NOTIFY_RECEIVER" "$NOTIFY_RECEIVER"
+      fi
+   fi
 fi
 
 
@@ -134,9 +134,9 @@ fi
 DIRS="etc/log  etc/tmp"
 
 for dir in $DIRS; do
-    dir="$PROJECT_DIR/$dir/"
-    [ -d "$dir" ] || mkdir -p "$dir"
-    chmod -R u+rwX,g+rwX "$dir"
+   dir="$PROJECT_DIR/$dir/"
+   [ -d "$dir" ] || mkdir -p "$dir"
+   chmod -R u+rwX,g+rwX "$dir"
 done
 
 USER="apache"
--- bin/admin/rsync.sh
+++ bin/admin/rsync.sh
@@ -9,19 +9,19 @@ SOURCE=$(readlink -e "$DB_FILE")
 
 # determine rsync target directory
 if [ $# -gt 0 ]; then
-    [ -z "$1" ]                     && { echo "error: invalid argument for rsync target"; exit 1; }
-    TARGET="$1"
+   [ -z "$1" ] && { echo "error: invalid argument for rsync target"; exit 1; }
+   TARGET="$1"
 else
-    [ -z ${ROST_RSYNC_TARGET+x} ] && { echo "error: missing argument or env ROST_RSYNC_TARGET for rsync target"; exit 1; }
-    [ -z ${ROST_RSYNC_TARGET}   ] && { echo "error: invalid environment setting ROST_RSYNC_TARGET"; exit 1; }
-    TARGET="$ROST_RSYNC_TARGET"
+   [ -z ${ROST_RSYNC_TARGET+x} ] && { echo "error: missing argument or env ROST_RSYNC_TARGET for rsync target"; exit 1; }
+   [ -z ${ROST_RSYNC_TARGET}   ] && { echo "error: invalid environment setting ROST_RSYNC_TARGET";              exit 1; }
+   TARGET="$ROST_RSYNC_TARGET"
 fi
 [ "${TARGET: -1}" != "/" ] && TARGET="$TARGET/"
 
 
 # check required binaries
-command -v rsync >/dev/null || { echo "error: rsync binary not found"; exit 1; }
-command -v ssh   >/dev/null || { echo "error: ssh binary not found";   exit 1; }
+command -v rsync > /dev/null || { echo "error: rsync binary not found"; exit 1; }
+command -v ssh   > /dev/null || { echo "error: ssh binary not found";   exit 1; }
 
 
 # run rsync (TODO: externalize ownership settings)
