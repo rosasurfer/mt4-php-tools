5e8acb199ea895d592fdd08b44e054c8a3985d67
Peter Walther <pewa-github@rosasurfer.com>
2024-08-30 13:20:53 +0300

    move log directory to project root

--- app/init.php
+++ app/init.php
@@ -9,11 +9,11 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 require(($appRoot=dirname(__DIR__)).'/vendor/autoload.php');
 
 // php.ini settings
-PHP::ini_set('log_errors',        1                                );
-PHP::ini_set('error_log',         $appRoot.'/etc/log/php-error.log');
-PHP::ini_set('session.save_path', $appRoot.'/etc/tmp'              );
-PHP::ini_set('default_charset',  'UTF-8'                           );
-PHP::ini_set('memory_limit',     '256M'                            );
+PHP::ini_set('log_errors',        1                            );
+PHP::ini_set('error_log',         $appRoot.'/log/php-error.log');
+PHP::ini_set('session.save_path', $appRoot.'/etc/tmp'          );
+PHP::ini_set('default_charset',  'UTF-8'                       );
+PHP::ini_set('memory_limit',     '128M'                        );
 
 // create a new application
 return new Application([
