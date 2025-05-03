595fe516c98a63dc292df6542caa1f7f6516c84a
Peter Walther <pewa-github@rosasurfer.com>
2025-05-03 19:19:51 +0300

    update dependencies

--- config/services.php
+++ config/services.php
@@ -8,7 +8,7 @@ use rosasurfer\rt\lib\metatrader\MetaTrader;
 /**
  * Service configuration
  *
- * @see  https://github.com/rosasurfer/ministruts/blob/master/src/di/
+ * @see  https://github.com/rosasurfer/ministruts/blob/master/src/core/di/
  */
 return [
     Dukascopy::class  => Dukascopy::class,
