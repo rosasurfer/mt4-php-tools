3c8d9f9838d193bbbb9ca761f71d2fba39fb88a0
Peter Walther <pewa-github@rosasurfer.com>
2026-02-10 20:14:52 +0200

    update endpoint for GitHub build notifications

---
+++ app/controller/forms/BuildNotificationActionForm.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace rosasurfer\rt\controller\forms;
+
+use rosasurfer\ministruts\struts\ActionForm;
+
+use function rosasurfer\ministruts\strIsDigits;
+
+/**
+ * BuildNotificationActionForm
+ */
+class BuildNotificationActionForm extends ActionForm
+{
+    /** @var string - repository name */
+    protected string $repository;
+
+    /** @var string - build artifact id */
+    protected string $artifactId;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function populate(): void
+    {
+        $input = $this->request->input();
+
+        $this->repository = trim($input->get('repository', ''));
+        $this->artifactId = trim($input->get('artifact-id', ''));
+    }
+
+   /**
+     * {@inheritDoc}
+    */
+    public function validate(): bool
+    {
+        $request = $this->request;
+
+        $name = 'repository';
+        $repository = $this->repository;
+
+        if ($repository == '') {
+            $request->setActionError($name, "missing parameter \"$name\"");
+        }
+        elseif ($repository != 'rosasurfer/mt4-mql-framework') {
+            $request->setActionError($name, "invalid parameter \"$name\"");
+        }
+
+        $name = 'artifact-id';
+        $artifactId = $this->artifactId;
+
+        if ($artifactId == '') {
+            $request->setActionError($name, "missing parameter \"$name\"");
+        }
+        elseif (!strIsDigits($artifactId)) {
+            $request->setActionError($name, "invalid parameter \"$name\"");
+        }
+
+        return !$request->isActionError();
+    }
+}
