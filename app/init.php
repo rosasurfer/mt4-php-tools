3ac25b933a0a1612a45dc397aee091ffd49edd5d
Peter Walther <pewa-github@rosasurfer.com>
2026-02-16 02:13:41 +0200

    add global debug helpers

--- app/init.php
+++ app/init.php
@@ -5,51 +5,7 @@ use rosasurfer\ministruts\Application;
 use rosasurfer\ministruts\util\PHP;
 
 // class loader
-require(__DIR__.'/../vendor/autoload.php');
-
-if (!function_exists('ddd')) {
-    /**
-     * Helper for dump-driven development. Appends a stringified variable to a dump file. If the file doesn't exist it is created.
-     * Alias in the global namespace.
-     *
-     * @param  mixed  $var
-     * @param  bool   $reset    [optional] - whether to reset the dump file (default: no)
-     * @param  string $filename [optional] - custom dump filename, remembered (default: dirname(ini_get('error_log')).'/ddd.log')
-     *
-     * @return mixed - any value to make the call an expression
-     */
-    function ddd($var, bool $reset = false, string $filename = '') {
-        return \rosasurfer\ministruts\ddd($var, $reset, $filename);
-    }
-}
-
-if (!function_exists('echof')) {
-    /**
-     * Prints a variable in a formatted and pretty way.
-     * Alias in the global namespace.
-     *
-     * @param  mixed $var
-     *
-     * @return void
-     */
-    function echof($var): void {
-        \rosasurfer\ministruts\ddd($var);
-    }
-}
-
-if (!function_exists('toString')) {
-    /**
-     * Convert any variable to a nicely formatted string. Can also be used to format JSON strings.
-     * Alias in the global namespace.
-     *
-     * @param  mixed $var
-     *
-     * @return string
-     */
-    function toString($var): string {
-        return \rosasurfer\ministruts\toString($var);
-    }
-}
+require __DIR__.'/../vendor/autoload.php';
 
 $rootDir = dirname(__DIR__);
 
